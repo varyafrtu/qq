ba779cba29aaa9e574c6e3db6c414c575d162db2
varyafrtu <sartinovavara620@gmail.com>
2025-10-04 09:22:17 +0500

    fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,12 @@
-import { Inter } from "next/font/google";
-import { Lusitana } from "next/font/google";
-export const inter = Inter({ subsets: ["latin"] });
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
 
-export const lusitana = Inter({ subsets: ["latin"] });
+export const inter = Inter({
+  weight: ["400", "700"],
+  subsets: ['latin'], // Add subsets for Inter
+});
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ['latin'], // Add subsets for Lusitana
+});
